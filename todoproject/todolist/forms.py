be22bc103da757bafdb08659c0ec209760f74c80
KseniyaSaltik <irbis.mr@gmail.com>
2022-05-18 10:46:02 +0300

    adding a bootstrap class in the form fields of form

--- todoproject/todolist/forms.py
+++ todoproject/todolist/forms.py
@@ -6,4 +6,17 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = "__all__"
+
+    title = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control form-control-lg",
+                "placeholder": "Enter task...",
+            }
+        ),
+    )
+    completed = forms.CharField(
+        required=False,
+        widget=forms.widgets.CheckboxInput(attrs={"class": "form-check-input"}),
+    )
         
